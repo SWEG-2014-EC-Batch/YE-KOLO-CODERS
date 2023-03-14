aea928ffaf7e002670780a2aa3c445015156a9e4
Meklit Habtamu <124443510+meklithab@users.noreply.github.com>
2023-03-14 18:34:59 +0300

    Update Body Mass Index Calculator.cpp
    Signed-off-by: Meklit Habtamu <124443510+meklithab@users.noreply.github.com>

--- Meklit Habtamu/Body Mass Index Calculator.cpp
+++ Meklit Habtamu/Body Mass Index Calculator.cpp
@@ -19,43 +19,46 @@ int main()
 
         float BMI;
         BMI = Weight / (Height * Height);
-        cout << "your BMI is:" << BMI;
+        cout << "Your BMI is:" << BMI<<endl;
 
         char r;
         cout << "Please insert your gender (M) For male or (F)For female): ";
         cin >> r;
+
+         while (r!='F' && r!='f' && r!='m' && r!='M')
+        {
+            cout<<"Wrong input enter (M) or (F)"<<endl;
+            cin>>r;
+        }
         if (r =='M'|| r =='m')
         {
             if (BMI >= 19 && BMI <= 25)
             {
-                cout << "You are healthy";
+                cout << "You are healthy"<<endl;
             }
 
-            else if (BMI<=19)
+            else if (BMI<19)
             {
-                cout << "You are underweight";
+                cout << "You are underweight"<<endl;
             }
 
-            else if (BMI>=25)
+            else if (BMI>25)
             {
-                cout << "You are overweight";
+                cout << "You are overweight"<<endl;
             }
         }
         else if(r =='F' || r =='f')
         {
             if (BMI >= 18 && BMI <= 24)
-                cout << "You are healthy";
-            else if (BMI<=19)
-                cout << "You are underweight";
-            else if (BMI>=24)
-                cout << "You are overweight";
+                cout << "You are healthy"<<endl;
+            else if (BMI<18)
+                cout << "You are underweight"<<endl;
+            else if (BMI>24)
+                cout << "You are overweight"<<endl;
         }
-        else
-        {
-            cout<<"Wrong input enter (M) or (F)";
-        }
+
+
     }
 
     return 0;
 }
-

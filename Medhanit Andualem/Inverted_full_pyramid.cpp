49a2fc9c88090a28b1a0aec1d43a76b40f0ab9c5
Medhanit Andualem <medanandualem@gmail.com>
2023-03-15 20:13:25 +0100

    Create Inverted_full_pyramid.cpp
    Signed-off-by: Medhanit Andualem <medanandualem@gmail.com>

---
+++ Medhanit Andualem/Inverted_full_pyramid.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+int x, i, j;
+cout<<"Enter height";
+cin>>x;
+for(int i=x; i>=1; i--){
+for(int j=x; j>=1; j--){
+    if(i>=j)
+cout<<"* ";
+else
+ cout<<" ";
+}
+cout<<endl;
+}
+return 0;
+
+}

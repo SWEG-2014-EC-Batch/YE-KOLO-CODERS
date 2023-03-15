5700ff534786d29ee2ed728b5b867a51efdcd504
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2023-03-15 23:09:13 +0300

    Create Half Pyramid.cpp
    Signed-off-by: Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>

---
+++ Meklit Melkamu Ayele/Half Pyramid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  for(int i=6;i>=1;i--)
+{
+     for(int j=i;j>0;j--)
+     {
+        cout<<"*";
+     }
+    cout<<endl;
+}
+
+    return 0;
+}

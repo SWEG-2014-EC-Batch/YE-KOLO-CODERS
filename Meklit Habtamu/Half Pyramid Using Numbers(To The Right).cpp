bcb6738c717abfb50737e24155d7794a17c78429
Meklit Habtamu <124443510+meklithab@users.noreply.github.com>
2023-03-16 18:38:03 +0300

    Half Pyramid Using Numbers(To The Right).cpp
    Signed-off-by: Meklit Habtamu <124443510+meklithab@users.noreply.github.com>

---
+++ Meklit Habtamu/Half Pyramid Using Numbers(To The Right).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int size = 5;
+  
+  for (int i = 0; i < size; i++)
+  {
+     for (int j = 1; j < size - i; j++) {
+      cout << " ";
+    }
+ 
+    for (int k = 0; k <= i; k++)
+    {
+      cout << (k+1);
+    }
+    cout << "\n";
+  }
+  return 0;
+}

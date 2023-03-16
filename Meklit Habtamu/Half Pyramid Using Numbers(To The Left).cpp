5be903ab18705cfa9a4b89b3588ebfce1ab94e53
Meklit Habtamu <124443510+meklithab@users.noreply.github.com>
2023-03-16 18:14:18 +0300

    Half Pyramid Using Numbers(To The Left).cpp
    Signed-off-by: Meklit Habtamu <124443510+meklithab@users.noreply.github.com>

---
+++ Meklit Habtamu/Half Pyramid Using Numbers(To The Left).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int rows=5;
+
+    for(int i = 1; i <= rows; ++i) {
+        for(int j = 1; j <= i; ++j) {
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}

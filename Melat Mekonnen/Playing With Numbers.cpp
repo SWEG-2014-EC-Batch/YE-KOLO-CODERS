77ac6d8d7980c9afa62662646f3290292ceaa9fc
melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>
2023-03-16 17:11:00 +0300

    Create Playing With Numbers.cpp
    Signed-off-by: melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>

---
+++ Melat Mekonnen/Playing With Numbers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y,k,i;
+    cout<<"ENTER HEIGHT";
+    cin>>x;
+    cout<<"ENTER WIDTH";
+    cin>>y;
+    for(k=1;k<=x;k++)
+    {
+       for(i=1;i<=y;i++)
+    {
+      cout<<"*";
+    }
+      cout<<endl;
+    }
+
+
+
+
+
+
+
+
+
+    return 0;
+}

2f2a4756f804a8d6124b93201f5e512011f34cc3
Meklit Habtamu <124443510+meklithab@users.noreply.github.com>
2023-03-16 17:53:40 +0300

    Half Pyramid.cpp
    Signed-off-by: Meklit Habtamu <124443510+meklithab@users.noreply.github.com>

---
+++ Meklit Habtamu/Half Pyramid.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int row=5;
+ for (int i=1;i<=row;i++)
+ {
+     for(int j=1;j<=i;j++)
+     {
+         cout<<"*";
+     }
+     cout<<"\n";
+ }
+return 0;
+}

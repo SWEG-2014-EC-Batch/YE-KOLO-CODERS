160378edc8d20cd3f5ade66edbbebb4d52f72f0e
melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>
2023-03-16 17:15:28 +0300

    Create Reverse Numbers.cpp
    Signed-off-by: melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>

---
+++ Melat Mekonnen/Reverse Numbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream> 
+using namespace std; 
+ 
+int main() 
+{ 
+    int num, r,  reverse=0; 
+    cout<<"Enter a number"; 
+    cin>>num; 
+    while(num) 
+    { 
+    r=num%10; 
+    num=num/10; 
+    reverse=(reverse*10)+r; 
+    } 
+    cout<<"Reversed number="<<reverse; 
+    }

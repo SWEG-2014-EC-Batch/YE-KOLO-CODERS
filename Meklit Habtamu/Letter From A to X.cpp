e42687a2bf7d293be6d2c3479b6ed79a16c18ba5
Medhanit Andualem <medanandualem@gmail.com>
2023-03-16 19:47:30 +0100

    Update Letter From A to X.cpp
    Signed-off-by: Medhanit Andualem <medanandualem@gmail.com>

--- Meklit Habtamu/Letter From A to X.cpp
+++ Meklit Habtamu/Letter From A to X.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int main()
 {
     char alphabet='A';
-    for(int i=1;i<=4;i++)
-    {
+    for(int i=1;i<=4;i++) {
         for(int j=1;j<=6;j++)
             cout<<alphabet++;
         cout<<endl;

074447e9f1ff5f8686466c78c517e15d0263e8b4
Meklit Habtamu <124443510+meklithab@users.noreply.github.com>
2023-03-16 18:07:30 +0300

    Inverted Full Pyramid.cpp
    Signed-off-by: Meklit Habtamu <124443510+meklithab@users.noreply.github.com>

---
+++ Meklit Habtamu/Inverted Full Pyramid.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int rows=5;
+
+    for(int i = rows; i >= 1; --i) {
+        for(int space = 0; space < rows-i; ++space)
+            cout << "  ";
+
+        for(int j = i; j <= 2*i-1; ++j)
+            cout << "* ";
+
+        for(int j = 0; j < i-1; ++j)
+            cout << "* ";
+
+        cout << endl;
+    }
+
+    return 0;
+}

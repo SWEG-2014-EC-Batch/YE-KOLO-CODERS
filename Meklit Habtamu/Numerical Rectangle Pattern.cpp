4f2c6611cb2a73ab9d711d20734a551b932e72ac
Meklit Habtamu <124443510+meklithab@users.noreply.github.com>
2023-03-16 19:57:02 +0300

    Numerical Rectangle Pattern.cpp
    Signed-off-by: Meklit Habtamu <124443510+meklithab@users.noreply.github.com>

---
+++ Meklit Habtamu/Numerical Rectangle Pattern.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+cout<<"1  2  3  4  5"<<endl;
+cout<<"1  2  3  4  5"<<endl;
+cout<<"1  2  3  4  5"<<endl;
+cout<<"1  2  3  4  5"<<endl;
+cout<<"1  2  3  4  5"<<endl;
+
+  return 0;
+}

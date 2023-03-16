40aac21cbd1d0099b5a81617f56d974db198c507
Meklit Habtamu <124443510+meklithab@users.noreply.github.com>
2023-03-16 20:29:08 +0300

    Update Numerical Rectangle Pattern.cpp
    Signed-off-by: Meklit Habtamu <124443510+meklithab@users.noreply.github.com>

--- Meklit Habtamu/Numerical Rectangle Pattern.cpp
+++ Meklit Habtamu/Numerical Rectangle Pattern.cpp
@@ -2,12 +2,17 @@
 using namespace std;
 int main()
 {
+    int row[5][5]={{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5}};
 
-cout<<"1  2  3  4  5"<<endl;
-cout<<"1  2  3  4  5"<<endl;
-cout<<"1  2  3  4  5"<<endl;
-cout<<"1  2  3  4  5"<<endl;
-cout<<"1  2  3  4  5"<<endl;
+    for (int i=0;i<5;i++)
+    {
+        for (int j=0;j<5;j++)
+        {
+            cout<<row[i][j];
+        }
+        cout<<"\n";
+
+    }
 
   return 0;
 }

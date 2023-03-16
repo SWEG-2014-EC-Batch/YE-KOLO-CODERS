0d63544481500244738ada6b65dba0c8f6c62613
Marsilas <124493099+Marsill@users.noreply.github.com>
2023-03-16 15:04:01 -0500

    Add files via upload
    Signed-off-by: Marsilas <124493099+Marsill@users.noreply.github.com>

---
+++ Marsilas Wondimagegnehu/Print number in order.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int num=10;
+   for(int i=1;i<=4;i++)
+  {
+      for(int j=1;j<=10;j++)
+        {cout<<num++<<" ";}
+         cout<<endl;}
+  return 0;
+  }
+
+
+
+
+  
+

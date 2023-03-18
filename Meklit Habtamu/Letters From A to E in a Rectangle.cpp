db66eb8fc4f44864f4bd19f64195d3381dd8dd1e
Meklit Habtamu <124443510+meklithab@users.noreply.github.com>
2023-03-18 16:08:29 +0300

    Update Letters From A to E in a Rectangle.cpp
    Signed-off-by: Meklit Habtamu <124443510+meklithab@users.noreply.github.com>

--- Meklit Habtamu/Letters From A to E in a Rectangle.cpp
+++ Meklit Habtamu/Letters From A to E in a Rectangle.cpp
@@ -1,20 +1,15 @@
-#include <iostream>
-
 using namespace std;
 
 int main()
 {
-
-    for(int i=1;i<=5;i++)
-    {
+ for(int i=1;i<=5;i++)
+   {
       char alphabet='a';
-      
      for(int j=1;j<=5;j++)
      {
-       cout<<alphabet++;
+         cout<<'\t'<<alphabet++;
      }
          cout<<endl;
     }
-
     return 0;
 }

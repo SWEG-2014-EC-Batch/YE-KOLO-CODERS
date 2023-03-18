fd53f912bf958969ec0ced9824d0f80c1d428e25
Meklit Habtamu <124443510+meklithab@users.noreply.github.com>
2023-03-18 16:04:33 +0300

    Letters From A to E in a Rectangle Format.cpp
    Signed-off-by: Meklit Habtamu <124443510+meklithab@users.noreply.github.com>

---
+++ Meklit Habtamu/Letters From A to E in a Rectangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    for(int i=1;i<=5;i++)
+    {
+      char alphabet='a';
+      
+     for(int j=1;j<=5;j++)
+     {
+       cout<<alphabet++;
+     }
+         cout<<endl;
+    }
+
+    return 0;
+}

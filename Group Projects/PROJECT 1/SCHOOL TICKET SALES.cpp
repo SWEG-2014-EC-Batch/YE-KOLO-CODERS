6a4771b6a993f50293f17ba2a61568283462e2d4
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2023-04-17 13:45:32 +0300

    Update and rename SCHOOL TICKET SALES to SCHOOL TICKET SALES.cpp
    Signed-off-by: Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>

---
+++ Group Projects/PROJECT 1/SCHOOL TICKET SALES.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int ticket_number, person_code, entrance_fee, num_infants = 0, num_children = 0, num_teens = 0, num_adults = 0;
+    int total_people = 0, total_sales = 0;   //Declare and Initialize the required variables.
+
+
+    cout << "Enter the ticket number, followed by the person code (1, 2, 3, or 4):\n";
+    do {
+        cin >> ticket_number >> person_code; //accept ticket number and person code consecutively
+
+        if (cin.fail() || person_code <= 0)  // cin.fail() checks if the input operation failed
+        {
+            break;
+        }
+
+        if (ticket_number <= 0) //checks if the input value for person_code is less than or equal to zero.
+        {
+            break;
+        }
+
+        switch (person_code)
+        {
+            case 1:
+                num_infants++;
+                entrance_fee = 5;
+                break;
+            case 2:
+                num_children++;
+                entrance_fee = 10;
+                break;
+            case 3:
+                num_teens++;
+                entrance_fee = 20;
+                break;
+            case 4:
+                num_adults++;
+                entrance_fee = 20;
+                break;
+            default:
+                cout << "Invalid code" << endl;
+                return 0;
+        }

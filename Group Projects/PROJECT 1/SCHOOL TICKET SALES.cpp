701f7303da208e61e7826da6a1425746a05cc737
melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>
2023-04-17 14:48:16 +0300

    Update SCHOOL TICKET SALES.cpp
    Signed-off-by: melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>

--- Group Projects/PROJECT 1/SCHOOL TICKET SALES.cpp
+++ Group Projects/PROJECT 1/SCHOOL TICKET SALES.cpp
@@ -57,39 +57,4 @@ int main()
     cout<<"4(adult): "<<num_adults <<" people\n";
    cout<<"Total Attendees; "<<total_people;
    return 0;
-}#include <iostream>
-#include <string>
-using namespace std;
-
-int main() {
-  int n;
-  cout << "Enter a positive integer: ";
-  cin >> n;
-
-  if (n < 1) {
-    cout << "Invalid input. Please enter a positive integer.\n";
-    return 0;
-  }
-
-  int totalWidth = 38; //The Total width of the pattern
-
-  // Print the upper half of the pattern
-  for (int i = 0; i < n; i++) {
-    string leftStars = string(3 * n - 3 - 2 * i, '*');
-    string middleSpaces = string(4 * i + 2, ' ');
-    string rightStars = string(3 * n - 3 - 2 * i, '*');
-
-    cout << leftStars << middleSpaces << rightStars << endl;
-  }
-
-  // Print the lower half of the pattern
-  for (int i = n - 2; i >= 0; i--) {
-    string leftStars = string(3 * n - 3 - 2 * i, '*');
-    string middleSpaces = string(4 * i + 2, ' ');
-    string rightStars = string(3 * n - 3 - 2 * i, '*');
-
-    cout << leftStars << middleSpaces << rightStars << endl;
-  }
-
-  return 0;
 }

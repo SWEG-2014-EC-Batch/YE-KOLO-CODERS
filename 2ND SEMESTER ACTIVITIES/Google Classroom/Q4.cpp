206c7ec3add6a315aff82dd05631262fa7674d5d
Meklit Habtamu <124443510+meklithab@users.noreply.github.com>
2023-06-11 15:15:12 +0300

    Update Q4.cpp
    Signed-off-by: Meklit Habtamu <124443510+meklithab@users.noreply.github.com>

--- 2ND SEMESTER ACTIVITIES/Google Classroom/Q4.cpp
+++ 2ND SEMESTER ACTIVITIES/Google Classroom/Q4.cpp
@@ -19,9 +19,9 @@ int product(int count, ...)
 
 int main()
 {
-    int result1 = product(3, 2, 3, 4); // Output: 24
-    int result2 = product(2, 5, 10);   // Output: 50
-    int result3 = product(5, 1, 2, 3, 4, 5); // Output: 120
+    int result1 = product(3, 2, 3, 4); 
+    int result2 = product(2, 5, 10);   
+    int result3 = product(5, 1, 2, 3, 4, 5); 
     cout << "Result 1: " << result1 << endl;
     cout << "Result 2: " << result2 << endl;
     cout << "Result 3: " << result3 << endl;

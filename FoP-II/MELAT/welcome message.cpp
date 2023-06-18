7402a92d564bb8e14c431b19369b4b19eda77793
melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>
2023-06-18 08:01:40 -0500

    Create welcome message.cpp
    Signed-off-by: melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>

---
+++ FoP-II/MELAT/welcome message.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void welcome_message(){
+	string your_name;
+	cout<< "enter your name: ";
+	cin>>your_name;
+	cout<<" Hello, "<<your_name<<endl;
+	cout<<"Welcome to M.P";
+}
+
+int main(){
+ welcome_message();
+	return 0;
+}
+	

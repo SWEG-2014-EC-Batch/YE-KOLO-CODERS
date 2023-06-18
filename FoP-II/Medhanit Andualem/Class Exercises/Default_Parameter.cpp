ea54d0e4816c0966267df7fcf3b2866c314277f5
Medhanit Andualem <medanandualem@gmail.com>
2023-06-18 23:00:09 +0300

    Create Default_Parameter.cpp
    Signed-off-by: Medhanit Andualem <medanandualem@gmail.com>

---
+++ FoP-II/Medhanit Andualem/Class Exercises/Default_Parameter.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int max = 10);
+int fibonacci(int max)
+{
+    int a1 = 0;
+    int a2 = 1;
+    int next_term = 0;
+
+    cout << "Fibonacci Series: ";
+    cout << a1 << "," << a2 << ",";
+    for (int i = 1; i <= max; i++)
+    {
+
+        next_term = a1 + a2;
+        a1 = a2; 
+        a2 = next_term;  
+
+        cout << next_term << " ,";
+    }
+}
+int main()
+{
+    int num;
+    int choice;
+    cout << "Would You Like To Enter Maximum Number(Yes=1/N=0)?" << endl;
+    cin >> choice;
+    if (choice == 1)
+    {
+        cout << "Enter Maximum Number: " << endl;
+        cin >> num;
+        fibonacci(num);
+    }
+    else
+        fibonacci();
+}

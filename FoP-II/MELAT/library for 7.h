77135318640a41eb7634f5a6a27c5c77bd8ec522
melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>
2023-06-18 08:28:09 -0500

    Update library for 7.h
    Signed-off-by: melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>

--- FoP-II/MELAT/library for 7.h
+++ FoP-II/MELAT/library for 7.h
@@ -1,1 +1,17 @@
+#ifndef libraryFor7
+#define libraryFor7
 
+int product(int count, ...)
+{
+    int result = 1;
+    va_list args;
+    va_start(args, count);
+    for (int i = 0; i < count; i++)
+    {
+        int arg = va_arg(args, int);
+        result *= arg;
+    }
+    va_end(args);
+    return result;
+}
+#endif

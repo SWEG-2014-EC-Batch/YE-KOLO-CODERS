a9b5398b8570f50cc85f2a4d37d1707a520f0d61
Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>
2023-06-21 12:57:54 +0300

    Create ArrayswithFunction.cpp
    Signed-off-by: Meklit Melkamu Ayele <118449086+meklitayele@users.noreply.github.com>

---
+++ FoP-II/Meklit Melkamu/Class Activities/ArrayswithFunction.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void sum(double arr[],int size)
+{
+  cout<<"FIRST ELEMENT:"<<arr[0]<<endl;
+  cout<<"SECOND ELEMENT:"<<arr[size-1];
+  cout<<"THE SUM OF THE FIRST AND AST ELEMENTS IS:"<<arr[0]+arr[size-1];
+
+}
+int main()
+{
+  int size;
+  cout<<"ENTER THE SIZE OF THE ARRAY:";
+  cin>>size;
+  
+  double arr[size];
+  for( int i=0;i<=size;i++)
+  {
+    cout<<"ENTER THE"<<" "<<i+1<<"element";
+    cin>>arr[i];
+  }
+  sum( arr,size);
+
+  return 0;
+}

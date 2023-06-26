5d78b6b6c66868f3b40b0226462e69155714b519
melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>
2023-06-26 15:19:47 +0300

    Create Lower case conversion.cpp
    Signed-off-by: melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>

---
+++ FoP-II/MELAT/home work activities/Lower case conversion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "lower.h"
+using namespace std;
+
+
+int main() {
+    
+    char str[100];
+    cout << "ENTER A STRING:";
+    cin.getline(str,100);
+    tolowerCase(str);
+    cout <<"THE NEW SENTENCE IS:"<<str;
+    return 0;
+}

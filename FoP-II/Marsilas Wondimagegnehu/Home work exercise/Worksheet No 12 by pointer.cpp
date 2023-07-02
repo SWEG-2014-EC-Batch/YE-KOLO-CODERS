b7660e761d085d1446785893262d2a03dd0dbba3
Marsilas Wondimagegnehu <124493099+Marsill@users.noreply.github.com>
2023-07-02 21:02:53 +0300

    Add files via upload
    Signed-off-by: Marsilas Wondimagegnehu <124493099+Marsill@users.noreply.github.com>

---
+++ FoP-II/Marsilas Wondimagegnehu/Home work exercise/Worksheet No 12 by pointer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void calcQuotient(double n1, double n2, double *answer)
+{
+	*answer = n1 / n2;
+	*answer = *answer + 10;
+}
+
+
+int main(){
+      double num1 = 16, num2 = 8, quotient;
+      
+    cout <<"Before, main \n" << num1 <<endl << num2 << endl;
+    
+      calcQuotient(num1, num2, &quotient);
+    
+    cout <<"After, main \n"<< num1 <<endl << num2 <<endl << quotient;
+    return 0;
+}

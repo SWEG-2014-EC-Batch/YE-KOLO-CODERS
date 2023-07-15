02b6c21768569052fbc50da207b50b0a5bc1c1f1
melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>
2023-07-15 19:28:43 +0300

    Update PatternPrinter.cpp
    Signed-off-by: melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>

--- FoP-II/GROUP PROJECTS/PROJECT 2/PATTERN PRINTING/VERSION 3_User_Defined_Library.cpp/PatternPrinter.cpp
+++ FoP-II/GROUP PROJECTS/PROJECT 2/PATTERN PRINTING/VERSION 3_User_Defined_Library.cpp/PatternPrinter.cpp
@@ -1,1 +1,39 @@
-e.
+#include "PatternPrinter.h"
+#include <iostream>
+using namespace std;
+
+bool PatternPrinter::isValidInput() {
+  return n >= 1;
+}
+
+void PatternPrinter::printUpperPattern() {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < 3 * n - 3 - 2 * i; j++) {
+      cout << "*";
+    }
+    for (int j = 0; j < 4 * i + 2; j++) {
+      cout << " ";
+    }
+    for (int j = 0; j < 3 * n - 3 - 2 * i; j++) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
+
+void PatternPrinter::printLowerPattern() {
+  for (int i = n - 2; i >= 0; i--) {
+    for (int j = 0; j < 3 * n - 3 - 2 * i; j++) {
+      cout << "*";
+    }
+    for (int j = 0; j < 4 * i + 2; j++) {
+      cout << " ";
+    }
+    for (int j = 0; j < 3 * n - 3 - 2 * i; j++) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
+
+

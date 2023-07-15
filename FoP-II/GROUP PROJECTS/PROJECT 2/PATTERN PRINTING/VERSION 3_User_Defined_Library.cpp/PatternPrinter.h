77af0078b252f8b13d66aa4477f99971f8879398
Medhanit Andualem <medanandualem@gmail.com>
2023-07-15 19:45:21 +0300

    Update PatternPrinter.h
    Signed-off-by: Medhanit Andualem <medanandualem@gmail.com>

--- FoP-II/GROUP PROJECTS/PROJECT 2/PATTERN PRINTING/VERSION 3_User_Defined_Library.cpp/PatternPrinter.h
+++ FoP-II/GROUP PROJECTS/PROJECT 2/PATTERN PRINTING/VERSION 3_User_Defined_Library.cpp/PatternPrinter.h
@@ -1,1 +1,14 @@
-e
+#ifndef PATTERN_PRINTER_H
+#define PATTERN_PRINTER_H
+
+struct PatternPrinter {
+  int n;
+
+  bool isValidInput();
+  void printUpperPattern();
+  void printLowerPattern();
+};
+
+#endif
+
+

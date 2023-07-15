96fdfddec9c3ad6e664bcaec1c289408b78699fd
Marsilas Wondimagegnehu <124493099+Marsill@users.noreply.github.com>
2023-07-15 12:24:25 -0500

    Update main.cpp
    Signed-off-by: Marsilas Wondimagegnehu <124493099+Marsill@users.noreply.github.com>

--- FoP-II/GROUP PROJECTS/PROJECT 2/PATTERN PRINTING/VERSION 3_User_Defined_Library.cpp/main.cpp
+++ FoP-II/GROUP PROJECTS/PROJECT 2/PATTERN PRINTING/VERSION 3_User_Defined_Library.cpp/main.cpp
@@ -1,1 +1,24 @@
-e.
+#include <iostream>
+#include "PatternPrinter.h"
+using namespace std;
+
+int main() {
+  PatternPrinter printer;
+  bool validInput = false;
+
+  while (!validInput) {
+    cout << "Enter a positive integer: ";
+    cin >> printer.n;
+
+    if (printer.isValidInput()) {
+      validInput = true;
+    } else {
+      cout << "Invalid input. Please enter a positive integer.\n";
+    }
+  }
+
+  printer.printUpperPattern();
+  printer.printLowerPattern();
+
+  return 0;
+}
